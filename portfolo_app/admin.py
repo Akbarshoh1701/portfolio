201f3949fc8e171c24120ecef11ca971333793bf
Akbarshoh1701 <akbarsuvonov5157@gmail.com>
2024-01-22 18:11:24 +0500

    I added the models to the admin panel

--- portfolo_app/admin.py
+++ portfolo_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register([About, Fact, Skills, Programs, Resume, Sumary, Education, ProfessionalExperience, Portfolio,
+                     Services, TypeService,  Testimonials, Comment, Contact, ])
